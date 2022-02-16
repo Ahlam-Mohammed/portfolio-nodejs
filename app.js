18834c3cd9f7c8ac6b083449824c41f02b74c9ff
Ahlam-Mohammed <ahlam.m.alfashq@gmail.com>
2022-02-16 14:34:59 +0300

    portfolio

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app     = express();
+const port    = 8080;
+
+app.use(express.static(__dirname + '/public'));
+
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+
+// index page
+app.get('/', function(req, res) {
+    res.render('pages/index');
+});
+
+
+// any page dose not exist
+app.use(function(req,res) {
+    res.status(404).render('pages/error-404');
+});
+
+app.listen(port);
+console.log('Server is listening on port ' + port);
